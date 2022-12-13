4791305db940526d6ec08f94750cb821231d7880
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2022-12-13 15:22:02 +0200

    feat: implement start and finish app

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const ERROR_MESSAGE = 'Operation failed';
+export const INVALID_INPUT = 'Invalid input';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
-console.log('hi');
+import { cwd } from 'process';
+import { INVALID_INPUT } from './constants.js';
+
+
+
+
+
+const userData = {
+	username: '',
+}
+
+const sayWelcome = () => {
+	const [executor, file, ...rest] = process.argv;
+
+	const arrArguments = rest[0].split('=');
+
+	if (arrArguments[0] === '--username') {
+		userData.username = arrArguments[1];
+	} else {
+		userData.username = 'anonymous';
+	}
+
+	console.log(`Welcome to the File Manager, ${userData.username}!`);
+}
+
+const handlUserInput = (userInput) => {
+	const [command, ...arg] = userInput.trim().split(' ');
+
+	console.log('Current command is ', command);
+
+	switch (command) {
+		case 'up':
+			console.log('up');
+			break;
+
+
+		case '.exit':
+			console.log(`Thank you for using File Manager, ${userData.username}, goodbye!`);
+			process.exit();
+
+
+		default:
+			console.log(INVALID_INPUT);
+
+	}
+}
+
+
+const init = () => {
+	sayWelcome();
+
+	process.stdin.on('data', (input) => {
+		handlUserInput(input.toString());
+	})
+
+	//showCurrentDirectory
+
+	process.on('SIGINT', () => {
+		console.log(`Thank you for using File Manager, ${userData.username}, goodbye!`);
+		process.exit();
+	})
+
+
+	//console.log(cwd());
+}
+
+init();
